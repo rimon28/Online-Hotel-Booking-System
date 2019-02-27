98238370dbb8b1dfe2bce6c4ac9b0cbffb7ae357
rimon28 <45998548+rimon28@users.noreply.github.com>
2019-02-27 18:51:20 +0600

    This is php file

---
+++ aboutus.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<title>Home Page</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<?php
+		include "index2.php";
+	?>
+	<div id="heading">
+		<h1>Contact Us</h1>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-12">
+			<p id="contactP">
+				Email Address: rimon.kumar.cse@gmail.com_addref
+			</p>
+			<p id="contactP">
+				Phone Number: 01521213484
+			</p>
+			<p id="contactP">
+				Address: Dhaka
+			</p>
+		</div>
+	</div>
+
+
+
+</body>
+</html>
+
+</html>
